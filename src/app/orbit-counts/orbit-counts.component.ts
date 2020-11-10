d035bd5cc6d221efe3c22e4f5a6030dfeae4222f
Aimee Bhasme <aimee.rae612@gmail.com>
2020-11-10 11:30:22 -0600

    completed bonus

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
+
+  types: string[] = ["Space Debris", "Communication", "Probe", "Positioning", "Space Station", "Telescope"];
+
+  constructor() {
+
+   }
+
+  ngOnInit() {
+  }
+
+  satelliteCounts(currentType: string): number {
+    return this.satellites.filter(satellite => satellite.type === currentType).length;
+  }
+
+}
